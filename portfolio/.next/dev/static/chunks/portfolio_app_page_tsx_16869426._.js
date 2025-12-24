ad923ae67102a6bf3873b492fe34ad4f7f28178f
harshchorghe <harshchorghe7@gmail.com>
2025-12-24 23:18:23 +0530

    Stop tracking .next build artifacts

--- portfolio/.next/dev/static/chunks/portfolio_app_page_tsx_16869426._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/12b66_eb3001fd._.js",
-  "static/chunks/portfolio_f967c4a5._.js"
-],
-    source: "dynamic"
-});
